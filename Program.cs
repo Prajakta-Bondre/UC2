a46975008d2966b63835bd40673f36113e8be6c3
Prajakta-Bondre <radhabondre20@gmail.com>
2021-09-23 15:33:16 +0530

    changes made

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace EmployeeUC2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Employee = 1;
+            int EmployeeWage = 0;
+
+            if (Employee == 1)
+            {
+                Console.WriteLine("Employee is present");
+                EmployeeWage = 238 * 12;
+                Console.WriteLine("EmployeeWage = " + EmployeeWage);
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+                Console.WriteLine("No Wages calculated");
+            }
+        }
+
+                
+    }
+}
